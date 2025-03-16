3666c6cfc3695ab358910d5944bdcf9240b76e7a
Andrii <andristwan28@gmail.com>
2025-03-16 13:33:42 +0700

    first commit

---
+++ app/Filament/Resources/ApiKeyResource/Pages/CreateApikey.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Filament\Resources\ApikeyResource\Pages;
+
+use App\Filament\Resources\ApikeyResource;
+use Filament\Actions;
+use Filament\Resources\Pages\CreateRecord;
+
+class CreateApikey extends CreateRecord
+{
+    protected static string $resource = ApikeyResource::class;
+}
